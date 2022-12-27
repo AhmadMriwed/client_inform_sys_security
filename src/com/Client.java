b83dff27c763e970e82ae3dadf8286fafbff5b1b
ahmad mriwed <mr.ahmadmriwed@gmail.com>
2022-12-27 14:17:45 +0300

    @mriwed

--- src/com/Client.java
+++ src/com/Client.java
@@ -1,9 +1,11 @@
 package com;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import model.ClientModel;
+import request_response.Msg;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -29,7 +31,7 @@ public class Client {
 //
 //        }
 
-            Socket s = new Socket("localhost", 4999);
+        ///    Socket s = new Socket("localhost", 4999);
 
 //            PrintWriter pr = new PrintWriter(s.getOutputStream());
 //            Scanner sc= new Scanner(System.in); //System.in is a standard input stream
@@ -38,14 +40,49 @@ public class Client {
 //            pr.println(text);
 //            pr.flush();
 
-            InputStreamReader in = new InputStreamReader(s.getInputStream());
-            BufferedReader bf =  new BufferedReader(in);
+      ///      InputStreamReader in = new InputStreamReader(s.getInputStream());
+       ///     BufferedReader bf =  new BufferedReader(in);
 
-            String str = bf.readLine();
-            System.out.println("client :"+ str );
-
-
+        ///    String str = bf.readLine();
+          ///  System.out.println("client :"+ str );
 
 
+
+        try {
+            // Create the socket
+            Socket clientSocket = new Socket("localhost",5000);
+            // Create the input & output streams to the server
+            ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+
+            // Read modify
+            // TODO here
+            Msg msg= new Msg();
+            msg.status=true;
+            msg.message="message";
+            msg.body=new ClientModel(999333999,"123456","lhma");
+            System.out.println(msg.toMap());
+            /* Send the Message Object to the server */
+            outToServer.writeObject(msg.toMap());
+
+            /* Retrive the Message Object from server */
+//            LinkedList<Message> inFromServerList = new LinkedList<>();
+//            Message msgFrmServer = null;
+//            inFromServerList = (LinkedList<Message>)inFromServer.readObject();
+//            msgFrmServer = inFromServerList.pop();
+
+            /* Print out the recived Message */
+//            System.out.println("Message: " + msgFrmServer.getMessage());
+//            System.out.println("Index: " + msgFrmServer.getIndex());
+//            System.out.println("Average: " + msgFrmServer.getAverage());
+
+
+            clientSocket.close();
+
+        } catch (Exception e) {
+            System.err.println("Client Error: " + e.getMessage());
+            System.err.println("Localized: " + e.getLocalizedMessage());
+            System.err.println("Stack Trace: " + e.getStackTrace());
+        }
     }
 }
